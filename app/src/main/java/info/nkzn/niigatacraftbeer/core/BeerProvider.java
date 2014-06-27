77008b0abc61fe1490d8bff299f42df04f904181
Yukiya Nakagawa <y.nakagawa@water-cell.jp>
2014-06-27 23:08:35 +0900

    rawに入れたJSON文字列を文字列として取り出せることを確認

--- app/src/main/java/info/nkzn/niigatacraftbeer/core/BeerProvider.java
+++ app/src/main/java/info/nkzn/niigatacraftbeer/core/BeerProvider.java
@@ -1,5 +1,14 @@
 package info.nkzn.niigatacraftbeer.core;
 
+import android.content.Context;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import info.nkzn.niigatacraftbeer.R;
+
 public class BeerProvider {
 
     private static BeerProvider instance;
@@ -13,4 +22,17 @@ public class BeerProvider {
         return instance;
     }
 
+    String readJsonAsset(Context context) throws IOException {
+        InputStream inputStream = context.getResources().openRawResource(R.raw.niigata_craft_beer_2014_json);
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+
+        StringBuilder sb = new StringBuilder();
+        String str;
+        while ((str = br.readLine()) != null) {
+            sb.append(str);
+        }
+
+        return sb.toString();
+    }
 }
